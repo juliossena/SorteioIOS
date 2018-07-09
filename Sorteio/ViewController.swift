fb6fba2d14a1be75494ada7b85267725d9bc4f09
Squad Tribanco <meumrv@meumrvs-Mac-mini.local>
2018-07-09 19:48:28 -0300

    final

--- Sorteio/ViewController.swift
+++ Sorteio/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultado: UILabel!
+    @IBAction func gerarNumero(_ sender: Any) {
+        let numero = arc4random_uniform(11)
+        
+        resultado.text = String(numero)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
