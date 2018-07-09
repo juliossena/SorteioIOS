70721d91a5fa81396ac64e28c60972c8cd03cf4b
Squad Tribanco <meumrv@meumrvs-Mac-mini.local>
2018-07-09 19:08:20 -0300

    inicio

---
+++ Sorteio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sorteio
+//
+//  Created by meumrv on 09/07/18.
+//  Copyright © 2018 teste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
